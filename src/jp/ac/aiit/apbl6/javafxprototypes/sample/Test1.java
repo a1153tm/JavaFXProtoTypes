06065efda58cb527de5a4c29f6fe9432353d2709
a1153tm <a1153tm@aiit.ac.jp>
2012-06-06 18:00:58 +0900

    1st page designed

--- src/jp/ac/aiit/apbl6/javafxprototypes/sample/Test1.java
+++ src/jp/ac/aiit/apbl6/javafxprototypes/sample/Test1.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import jp.ac.aiit.apbl6.javafxprototypes.activity.Activity;
@@ -13,15 +14,22 @@ import jp.ac.aiit.apbl6.javafxprototypes.browser.RequestHyperLink;
 
 public class Test1 extends Activity
 {
+    //@FXML
+    
     @FXML
-    VBox vbox;
-        
+    private RadioButton radioA;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        vbox.getChildren().add(new RequestHyperLink("/test2", "go to Test2"));
     }
 
     @Override
     public void setParent(Activity parent) {
+    }
+    
+    @FXML
+    public void testButtonClick(ActionEvent event)
+    {
+        System.out.println("ok");
     }
 }
